833f84608fb94ef54ecbda1f0413bd8eca8bf0f8
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-05-16 20:21:11 +0530

    Added: UC2_Ability to add contacts to Address Book

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -4,5 +4,8 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.addPerson();
     }
 }
